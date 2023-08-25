607a5462ce62958859c1b1383d83b8c6f59a3793
AayushG02 <aayush.girdhar2@gmail.com>
2023-08-25 21:53:26 +0530

    minor changes

--- src/pages/ChartsMaps.tsx
+++ src/pages/ChartsMaps.tsx
@@ -2,7 +2,7 @@ import LineGraph from "../components/LineGraph";
 import Map from "../components/Map";
 const ChartsMaps = () => {
   return (
-    <div className="flex-1 flex flex-col gap-8 h-full">
+    <div className="flex-1 flex flex-col gap-20 h-full">
       <LineGraph />
       <Map />
     </div>
